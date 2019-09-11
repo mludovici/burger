fb9e75f005ce54af7c7c369d73ae74b68086d7e6
Marc <ludovicimarc@gmail.com>
2019-09-11 10:25:06 +0200

    added Build folder

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,7 +5,7 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 const burger = (props) => {
     let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, i) => {                
+            return [...Array(props.ingredients[igKey])].map((test, i) => {                
                 return <BurgerIngredient key={igKey + i} type={igKey} />
             });
         }).reduce((arr,el) => {
